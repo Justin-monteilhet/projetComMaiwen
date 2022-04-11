faefd25c378f39e91f5dcb5f71b36ec82d99b44b
KRandom <monteilhet.justin1702@gmail.com>
2022-04-11 17:54:31 +0200

    from 3 api requests to 2, session tiemout to reset tokens

--- ED_api.py
+++ ED_api.py
@@ -24,7 +24,13 @@ class Transaction:
 
     @classmethod
     def from_json(cls, d:dict):
+        """Creates a Transaction instance from the JSON response of api.ecoledirecte.com"""
         return cls(d['montant'], d['date'], d['libelle'])
+    
+    @classmethod
+    def from_dict(cls, d:dict):
+        """Creates a Transaction instance from a Transition.to_json() dict"""
+        return cls(d['value'], d['date'], d['label'])
 
 class URL:
     login = "https://api.ecoledirecte.com/v3/login.awp?v=4.6.0"
@@ -106,63 +112,3 @@ class APISession:
         headers = {'x-token' : self.token}
         r = self._session.post(url, headers=headers, data="data={}")
         return r.json()
-
-
-def main():
-    s = rq.Session()
-    
-    login_url = "https://api.ecoledirecte.com/v3/login.awp?v=4.6.0"
-    timeline_url = "https://api.ecoledirecte.com/v3/1/2830/timelineAccueilCommun.awp?verbe=get&v=4.6.0  "
-
-    username = input("Nom d'utilisateur du compte parent : ")
-    password = getpass("Mot de passe du compte parent : ")
-
-    form_data = {
-        "identifiant" : username,
-        "motdepasse" : password
-    }
-
-    r = s.post(login_url, data=f"data={json.dumps(form_data)}" )
-
-    response = r.json()
-    token = response.get('token')
-
-    r = s.post(timeline_url, headers={'x-token' : token}, data='data={}')
-
-    timeline_data = r.json().get('data')
-
-    with open('timeline.json', 'w') as f:
-        f.write(json.dumps(timeline_data, indent=2))
-
-    accounts = timeline_data["comptes"]["comptes"]
-    accounts = [acc for acc in accounts if acc["typeCompte"] == "portemonnaie"]
-    
-    names = {}  # bindes names to account index in accounts
-    for i, acc in enumerate(accounts):
-        libelle = acc.get("libelle")  # for example PM_RESTO JAMES
-        name = libelle[libelle.find(' ')+1:].lower()
-        names[name] = i
-    
-    for name, index in names.items():
-        print(f'{index+1}) {name.title()}')
-    
-    while 1:
-        user_index = input("Entrez le chiffre devant votre nom : ")
-        if user_index.isdigit() :
-            if 0 < int(user_index) <= len(names) :
-                break
-        print("Incorrect. Veuillez entrer le chiffre devant votre nom.")
-    
-    user_index = int(user_index)
-    user_index -= 1    # goes back from user input to correct index 
-
-    user_acc = accounts[user_index]
-    sold = user_acc.get("solde")
-    
-    print(f"Vous avez actuellement €{sold}.")
-
-    # https://api.ecoledirecte.com/v3/comptes/detail.awp?verbe=get&v=4.6.0
-
-
-
-if __name__ == '__main__' : main()
--- main.py
+++ main.py
@@ -1,36 +1,47 @@
+import logging
+from datetime import timedelta
+from typing import List
+
 from flask import Flask, render_template, request, session
 
-from ED_api import APISession
+from ED_api import APISession, Transaction
 
 app = Flask(__name__)
 app.secret_key = "clé-très-secrète"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if "ed-token" in session and "name" in session :    # if logged in, redirects to logs
-        logs = APISession(session['ed-token']).sold_logs
-        user_logs = logs[session['name']]
-        print("session found, rendering logs")
-        return render_template('logs.html', logs=user_logs)
+    # if "ed-token" in session and "name" in session :    # if logged in, redirects to logs
+    #     logs = APISession(session['ed-token']).sold_logs
+    #     user_logs = logs[session['name']]
+    #     print("session found, rendering logs")
+    #     return render_template('logs.html', logs=user_logs)
 
     form = request.form 
 
     if "username" in form and "password" in form:   # if sending a login request, login and redirects to choiceStudent
         username, password = form['username'], form['password']
         api = APISession.from_credentials(username, password)
-        session['ed-token'] = api.token
-        names = api.names
-        print("credentials found, rendering studentChoice")
+        sold_logs = api.sold_logs
+        names = list(sold_logs.keys())  # key of sold logs are the students' names
+        for name, trans_list in sold_logs.items():
+            sold_logs[name] = [trans.to_json() for trans in trans_list]
+
+        session['logs'] = sold_logs
+
+        logging.info("credentials found, rendering studentChoice")
         return render_template("studentChoice.html", names=names)
     
-    if "name" in form and 'ed-token' in session:  # if sending the student name, redirect to logs
+    if "name" in form and 'logs' in session:  # if sending the student name, redirect to logs
         name = form['name']
-        session['name'] = name
-        user_logs = APISession(session['ed-token']).sold_logs.get(name)
-        print("name found, rendering logs")
+        raw_user_logs : List[dict] = session["logs"].get(name)
+
+        user_logs = list(map(Transaction.from_dict, raw_user_logs))
+        logging.info("name found, rendering logs")
         return render_template('logs.html', logs=user_logs)
 
-    print("Nothing found, rendering default login")
+    logging.info("No data in form/session, rendering default login")
     return render_template("login.html")    # default
 
 app.run()
