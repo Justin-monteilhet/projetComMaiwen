23ed7b89658d4b05d31643d352ba4deff8800b58
Blyhacks <blyhacks@gmail.com>
2022-04-14 20:50:19 +0200

    Ajout du front-end au formulaire

---
+++ ED_api.py
@@ -0,0 +1,119 @@
+from ast import Str
+from typing import Dict, List
+import json
+from collections import namedtuple
+from datetime import datetime
+import requests as rq
+from getpass import getpass
+
+from ftfy import fix_text
+
+Credentials = namedtuple('Credentials', ['username', 'password'])
+Credentials.__doc__ = "Describes a 2-variable tuple containing a username and a password"
+
+class IncorrectCredentialsError(Exception) : ...
+
+class Transaction:
+    def __init__(self, value:int, date:str, label:str) -> None:
+        self.value, self.label = value, fix_text(label)
+        self.date = datetime.strptime(date, "%Y-%m-%d")
+
+    def to_json(self) -> dict:
+        return {
+            "value" : self.value,
+            "label" : self.label,
+            "date" : self.date.strftime("%Y-%m-%d")
+        }
+
+    @classmethod
+    def from_json(cls, d:dict):
+        """Creates a Transaction instance from the JSON response of api.ecoledirecte.com"""
+        return cls(d['montant'], d['date'], d['libelle'])
+    
+    @classmethod
+    def from_dict(cls, d:dict):
+        """Creates a Transaction instance from a Transition.to_json() dict"""
+        return cls(d['value'], d['date'], d['label'])
+
+class URL:
+    login = "https://api.ecoledirecte.com/v3/login.awp?v=4.6.0"
+    timeline = "https://api.ecoledirecte.com/v3/1/2830/timelineAccueilCommun.awp?verbe=get&v=4.6.0"
+    detail = "https://api.ecoledirecte.com/v3/comptes/detail.awp?verbe=get&v=4.6.0"
+
+class APISession:
+    def __init__(self, token:str) -> None:
+        self.token = token
+        self._session = rq.Session()
+
+    @classmethod
+    def from_credentials(cls, username:str, password:str):
+        login_credentials = "data=" + json.dumps({"identifiant" : username, "motdepasse" : password})
+        login = rq.post(URL.login, data=login_credentials)
+        login_data = login.json()
+        if login_data.get("code") == 505:
+            raise IncorrectCredentialsError(f"Username \"{username}\" and password \"{password}\" do not match.")
+
+        return cls(login_data.get("token"))
+
+    @property
+    def names(self) -> List[str]:
+        """Names of the students associated to the parent account."""
+        accs_data = self._request_token(url=URL.timeline)
+
+        accounts = [acc for acc in accs_data['data']['comptes']['comptes'] if acc["typeCompte"] == "portemonnaie"]
+        names = []
+        for acc in accounts:
+            libelle = acc.get("libelle")  # for example PM_RESTO JAMES
+            name = libelle[libelle.find(' ')+1:].lower()
+            names.append(fix_text(name))
+            
+        return names
+
+    @property
+    def solds(self) -> dict:
+        """Returns a dict binding student name to his account sold"""
+        accs_data = self._request_token(url=URL.timeline)
+
+        accounts = [acc for acc in accs_data['data']['comptes']['comptes'] if acc["typeCompte"] == "portemonnaie"]
+        solds = {}
+        for acc in accounts:
+            sold = acc['solde']
+            libelle = acc.get("libelle")  # for example PM_RESTO JAMES
+            name = fix_text(libelle[libelle.find(' ')+1:].lower())
+            solds[name] = sold
+            
+        return solds
+
+    @property
+    def sold_logs(self) -> Dict[str, List[Transaction]]:
+        detailed_data = self._request_token(url=URL.detail)
+        accounts = [acc for acc in detailed_data['data']['comptes'] if acc['typeCompte'] == "portemonnaie"]
+        
+        all_transactions = {}   # student name -> List of his transactions json formatted
+        for account in accounts:
+            libelle = account.get("libelle") 
+            name = fix_text(libelle[libelle.find(' ')+1:].lower())
+            transactions = []
+
+            for trans in account['ecritures']:
+                if not trans['montant'] : continue
+                if trans['montant'] > 0:
+                    transactions.append(Transaction.from_json(trans))
+                    continue
+
+                # if here, this trans is negative and groups all the purchases
+                for purchase in trans['ecritures']:
+                    purchase['montant'] *= -1   # transaction becomes negative
+                    transactions.append(Transaction.from_json(purchase))
+
+            # sort transactions by datetime
+            transactions.sort(key=lambda x:x.date, reverse=True)
+            all_transactions[name] = transactions
+        
+        return all_transactions
+    
+
+    def _request_token(self, url):
+        headers = {'x-token' : self.token}
+        r = self._session.post(url, headers=headers, data="data={}")
+        return r.json()
---
+++ main.py
@@ -0,0 +1,51 @@
+import json
+import logging
+from datetime import timedelta
+from typing import List
+
+from flask import Flask, render_template, request, session
+
+from ED_api import APISession, Transaction, IncorrectCredentialsError
+
+app = Flask(__name__)
+app.secret_key = "clé-très-secrète"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # if "ed-token" in session and "name" in session :    # if logged in, redirects to logs
+    #     logs = APISession(session['ed-token']).sold_logs
+    #     user_logs = logs[session['name']]
+    #     print("session found, rendering logs")
+    #     return render_template('logs.html', logs=user_logs)
+
+    form = request.form 
+
+    if "username" in form and "password" in form:   # if sending a login request, login and redirects to choiceStudent
+        username, password = form['username'], form['password']
+        try : 
+            api = APISession.from_credentials(username, password)
+
+        except IncorrectCredentialsError :
+            return render_template("login.html", code=401)
+
+        sold_logs = api.sold_logs
+        names = list(sold_logs.keys())  # key of sold logs are the students' names
+        for name, trans_list in sold_logs.items():
+            sold_logs[name] = [trans.to_json() for trans in trans_list]
+
+        logging.info("credentials found, rendering studentChoice")
+        return render_template("studentChoice.html", names=names, logs=json.dumps(sold_logs))
+    
+    if "name" in form and "logs" in form:  # if sending the student name, redirect to logs
+        name = form['name']
+        raw_user_logs : List[dict] = json.loads(form['logs']).get(name)
+
+        user_logs = list(map(Transaction.from_dict, raw_user_logs))
+        logging.info("name found, rendering logs")
+        return render_template('logs.html', logs=user_logs)
+
+    logging.info("No data in form/session, rendering default login")
+    return render_template("login.html", code=200)    # default
+
+app.run()
